38a65afae0ad33cb6b74c376a50a09affd0cbfa7
T-Y <takayukiyamazaki0227@gmail.com>
2021-09-02 20:25:23 +0900

    09-02

---
+++ test.bat
@@ -0,0 +1,6 @@
+rem https://www.k-tanaka.net/cmd/xcopy.php
+xcopy 20210901 20210831 /Y /I
+xcopy 20210901 20210830 /Y /I
+xcopy 20210901 20210829 /Y /I
+xcopy 20210901 20210828 /Y /I
+xcopy 20210901 20210827 /Y /I
